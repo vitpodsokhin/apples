9fff4d897aeddea3a1dcacb3e2884cf44a015e77
v <vitpodsokhin@gmail.com>
2023-12-21 14:42:12 +0300

    test netstat parsing

--- run-netstat.py
+++ run-netstat.py
@@ -2,5 +2,14 @@
 import subprocess
 
 netstat_command = 'netstat -nval'
-run_args = "shell=True, capture_output=True, encoding='utf8', text=True".split(', ')
-print(subprocess.run(netstat_command, **run_args).stdout)
+run_args_str = "shell=True, capture_output=True, encoding='utf8', text=True"
+
+run_args = {}
+for arg in run_args_str.split(', '):
+    key, value = arg.split('=')
+    run_args[key] = eval(value)
+
+netstat_out = subprocess.run(netstat_command, **run_args).stdout
+netstat_lines = netstat_out.splitlines()
+for line in netstat_lines:
+    print(line)
