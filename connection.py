6aa3da17b25403766d508ce4f41b68231d110e64
vitpodsokhin <vitpodsokhin@gmail.com>
2023-12-23 02:30:09 +0300

    refactor

--- connection.py
+++ connection.py
@@ -1,12 +1,14 @@
 import subprocess
 import json
 from dataclasses import dataclass
+from sys import stdout
 
-subprocess_run_args_str = "shell=True, capture_output=True, encoding='utf8', text=True"
+subprocess_run_args_str = "shell=True, capture_output=True, text=True, encoding='utf8'"
 subprocess_run_args = {}
 for arg in subprocess_run_args_str.split(', '):
     key, value = arg.split('=')
     subprocess_run_args[key] = eval(value)
+del subprocess_run_args_str
 
 @dataclass
 class BaseConnection:
@@ -16,43 +18,51 @@ class BaseConnection:
     localSocket: str
     remoteSocket: str
 
-
     def process_socket(self, socket_attr, socket_str, socket_type):
+
         address = '.'.join(socket_str.split('.')[:-1])
         port = socket_str.split('.')[-1]
         if socket_str != '*.*':
             setattr(self, socket_attr, f"{address}:{port}")
             setattr(self, f"{socket_type}Addr", address)
             setattr(self, f"{socket_type}Port", port)
+
+        #TODO change the asterisks in the addresses to 0.0.0.0 or :: 
+            # depending on the network protocol family
         # elif address == '*': # and port != '*':
         #     setattr(self, socket_attr, f"0.0.0.0:{port}")
         #     setattr(self, f"{socket_type}Addr", '0.0.0.0')
         #     setattr(self, f"{socket_type}Port", port)
 
     def __post_init__(self):
+
+        self.pid = int(self.pid)
+        self.family = 4 if self.proto.endswith('4') else 6
         self.process_socket("localSocket", self.localSocket, "local")
         self.process_socket("remoteSocket", self.remoteSocket, "remote")
         result = subprocess.run(f"ps -p {self.pid} -o command", **subprocess_run_args)
         self.command_line = result.stdout.splitlines()[1]
 
-    def as_dict(self):
+    def as_dict(self) -> dict:
         connection_dict = self.__dict__
         return connection_dict
 
-    def to_json(self):
-        connection = {
+    def to_dict(self) -> json:
+        connection_dict = {
             'pid': self.pid,
-            'proto': self.proto,
             'command_line': self.command_line,
+            'family': self.family,
+            'proto': self.proto,
             'localSocket': self.localSocket,
             'remoteSocket': self.remoteSocket
         }
-        connection_json = json.dumps(connection)
-        return connection_json
+        return connection_dict
 
 @dataclass
 class TCP_Connection(BaseConnection):
+    '''... inherits class BaseConnection'''
     state: str
+    #TODO following are common for both -- there must be a way to ...
     rhiwat: int
     shiwat: int
     pid: int
@@ -65,10 +75,13 @@ class TCP_Connection(BaseConnection):
     usscnt: int
     rtncnt: int
     fltrs: int
-    command_line: str = None
+    family: int = None
+    command_line: str = ''
 
 @dataclass
 class UDP_Connection(BaseConnection):
+    '''... inherits class BaseConnection'''
+    #TODO ... to reduce the code repetition.
     rhiwat: int
     shiwat: int
     pid: int
@@ -81,4 +94,10 @@ class UDP_Connection(BaseConnection):
     usscnt: int
     rtncnt: int
     fltrs: int
-    command_line: str = None
+    family: str = None
+    command_line: str = ''
+
+@dataclass
+class Process:
+    pid: int
+    command_line: str
--- run-netstat.py
+++ run-netstat.py
@@ -1,43 +1,64 @@
 #!/usr/bin/env python3
-import subprocess
+import subprocess, json
 from connection import TCP_Connection, UDP_Connection, subprocess_run_args
 
+families = ('inet', 'inet6')
 protos = ('tcp', 'udp')
 
-def run_netstat(proto:protos=None) -> list[str]:
-    netstat_command = f"netstat -nval {'-p '+proto if proto is not None else ''}"
+def run_netstat(
+        family:families=None,
+        proto:protos=None
+    ) -> list[str]:
+
+    family_selector = '-f '+family if family is not None else ''
+    proto_selector = '-p '+proto if proto is not None else ''
+
+    netstat_command = f"netstat -nval {family_selector} {proto_selector}"
     netstat_out = subprocess.run(netstat_command, **subprocess_run_args).stdout
     netstat_lines = netstat_out.splitlines()
+
     return netstat_lines
 
-def parse_netstat_connection(netstat_connection_line:str) -> TCP_Connection|UDP_Connection:
+def parse_netstat_connection(
+        netstat_connection_line:str
+    ) -> TCP_Connection|UDP_Connection:
+
     connection_classes = {'tcp': TCP_Connection, 'udp': UDP_Connection}
     proto = netstat_connection_line.split()[0]
+
     for protocol_name in protos:
         if proto.startswith(protocol_name):
             proto = protocol_name
     Parse_Connection = connection_classes[proto]
     connection = Parse_Connection(*netstat_connection_line.split())
+
     return connection
 
-def parse_netstat_connections(proto:protos=None, netstat_lines:str=None) -> list[TCP_Connection|UDP_Connection]:
+def parse_netstat_connections(
+        family:families=None,
+        proto:protos=None,
+        netstat_lines:str=None
+    ) -> list[TCP_Connection|UDP_Connection]:
+
     if netstat_lines == None:
-        netstat_lines = run_netstat(proto)
+        netstat_lines = run_netstat(family, proto)
+
     connections = []
     for line in netstat_lines:
         if line.startswith(protos):
             connection = parse_netstat_connection(line)
             connections.append(connection)
-    return(connections)
 
-from pprint import pprint
+    return connections
+
 def main():
-    connections = parse_netstat_connections('udp')
+    connections = parse_netstat_connections()
+    connections_list = []
     for connection in connections:
-        # if connection.state == 'LISTEN':
-        # if connection.localSocket.endswith('9999') or connection.remoteSocket.endswith('9999'):
-            connection_dict = connection.as_dict()
-            print(connection_dict)
+        connection_dict = connection.to_dict()
+        connections_list.append(connection_dict)
+    
+    print(json.dumps(connections_list))
 
 if __name__ == '__main__':
     main()
