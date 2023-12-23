bd8d96c595ae1e703674bec9b6178e94fb7587c4
vitpodsokhin <vitpodsokhin@gmail.com>
2023-12-23 04:12:08 +0300

    add comments to functions of the `run-netstat.py` main executable

--- run-netstat.py
+++ run-netstat.py
@@ -6,12 +6,21 @@ families = ('inet', 'inet6')
 protos = ('tcp', 'udp')
 
 def run_netstat(
-        family:families=None,
-        proto:protos=None
+        family: families = None,
+        proto: protos = None
     ) -> list[str]:
+    """
+    Executes the 'netstat' command to retrieve network connection information.
 
-    family_selector = '-f '+family if family is not None else ''
-    proto_selector = '-p '+proto if proto is not None else ''
+    Parameters:
+    - family: Optional[str] - The network family to filter (options: 'inet', 'inet6').
+    - proto: Optional[str] - The protocol to filter (options: 'tcp', 'udp').
+
+    Returns:
+    - list[str]: A list of strings containing the output lines of the 'netstat' command.
+    """
+    family_selector = '-f ' + family if family is not None else ''
+    proto_selector = '-p ' + proto if proto is not None else ''
 
     netstat_command = f"netstat -nval {family_selector} {proto_selector}"
     netstat_out = subprocess.run(netstat_command, **subprocess_run_args).stdout
@@ -20,9 +29,17 @@ def run_netstat(
     return netstat_lines
 
 def parse_netstat_connection(
-        netstat_connection_line:str
-    ) -> TCP_Connection|UDP_Connection:
+        netstat_connection_line: str
+    ) -> TCP_Connection | UDP_Connection:
+    """
+    Parses a single line of 'netstat' output to create a TCP or UDP connection object.
 
+    Parameters:
+    - netstat_connection_line: str - A line of 'netstat' output representing a network connection.
+
+    Returns:
+    - TCP_Connection or UDP_Connection: An instance of the appropriate connection class.
+    """
     connection_classes = {'tcp': TCP_Connection, 'udp': UDP_Connection}
     proto = netstat_connection_line.split()[0]
 
@@ -35,12 +52,22 @@ def parse_netstat_connection(
     return connection
 
 def parse_netstat_connections(
-        proto:protos=None,
-        family:families=None,
-        netstat_lines:str=None
-    ) -> list[TCP_Connection|UDP_Connection]:
+        proto: protos = None,
+        family: families = None,
+        netstat_lines: str = None
+    ) -> list[TCP_Connection | UDP_Connection]:
+    """
+    Parses 'netstat' output lines to create a list of TCP or UDP connection objects.
 
-    if netstat_lines == None:
+    Parameters:
+    - proto: Optional[str] - The protocol to filter (options: 'tcp', 'udp').
+    - family: Optional[str] - The network family to filter (options: 'inet', 'inet6').
+    - netstat_lines: Optional[list[str]] - List of 'netstat' output lines.
+
+    Returns:
+    - list[TCP_Connection or UDP_Connection]: A list of connection objects.
+    """
+    if netstat_lines is None:
         netstat_lines = run_netstat(family, proto)
 
     connections = []
@@ -57,7 +84,7 @@ def main():
     for connection in connections:
         connection_dict = connection.to_dict()
         connections_list.append(connection_dict)
-    
+
     print(json.dumps(connections_list))
 
 if __name__ == '__main__':
