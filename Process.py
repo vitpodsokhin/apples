e5c58077d2eda0d99db61366500c1199be17b1e7
vitpodsokhin <vitpodsokhin@gmail.com>
2023-12-24 19:49:39 +0300

    refactor the way the connections of a process are collected

--- Process.py
+++ Process.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from subprocess import run
 
 from Common import subprocess_run_args
+from Netstat import Netstat
 
 @dataclass
 class Process:
@@ -18,3 +19,13 @@ class Process:
     @property
     def as_dict(self):
         return self.__dict__
+
+    def get_connections_of_process(process, connections=None) -> dict:
+        if connections is None:
+            connections = Netstat.get_connections()
+        connections_of_process = [connection for connection in connections if connection.pid == process.pid]
+        return {
+            str(process.pid): process.as_dict,
+            'connections_count': len(connections_of_process),
+            'connections': [connection.to_dict() for connection in connections_of_process]
+        }
